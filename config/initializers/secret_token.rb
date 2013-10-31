fb79100b8424a2fd15bc3eb9e6ac2717824f8b2c
farmager <farmagerproject@gmail.com>
2013-10-31 11:11:41 -0600

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Farmagerproject::Application.config.secret_key_base = 'f44e4799322e617452d0d648cb7ed32d78256aeb892d4701b62db33e89c8dafac93055c86504bc5254d2df0cd732195c6d6f1118fa8d10ec978069ff860a391a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Farmagerproject::Application.config.session_store :cookie_store, key: '_farmagerproject_session'
