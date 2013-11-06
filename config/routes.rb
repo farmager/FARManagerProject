ef0e2a6bfc7cd3e820b0a54a64fd67cec8c68229
farmager <farmagerproject@gmail.com>
2013-11-06 11:39:37 -0600

    User authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Farmagerproject::Application.routes.draw do
  
   
   # get "users/new"
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
-  # match '/home',    to: 'static_pages#home',    via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/faq',    to: 'static_pages#faq',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contactus', to: 'static_pages#contactus', via: 'get'
