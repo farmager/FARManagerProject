50cfc11dd5d05b37a0368a16b1fcc14078ea3029
farmager <farmagerproject@gmail.com>
2014-02-23 02:09:14 -0600

    Problem with another migration

--- db/migrate/20140218204612_change_crop_table.rb
+++ db/migrate/20140218204612_change_crop_table.rb
@@ -1,19 +1,6 @@
 class ChangeCropTable < ActiveRecord::Migration
   def change
-    croptype
-    variety
-       t.string   "herbicides"
-    t.string   "fungicides"
-    t.integer  "actual_seeded"
-    t.integer  "total_produced"
-    t.integer  "yield"
-    t.integer  "field_id"
-    t.integer  "grade"
-    t.integer  "nitrogen"
-    t.integer  "phosphorous"
-    t.integer  "potassium"
-    t.integer  "sulfur"
-    t.integer  "years"
-    t.date     "date_seeded"
+   
+ 
   end
 end
